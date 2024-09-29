f5ed7f2cf99ea12758e4e58b4dc1321440e2cce7
JuanDev <lemniscatachigne@gmail.com>
2024-09-29 13:00:29 -0300

    Gif basado en tres palabras

---
+++ README.md
@@ -0,0 +1,16 @@
+Identificando lo que tenemos que hacer
+
+API de datos de gato
+
+Crear una pagina web que me muestre git aleatorios atreves de tres palabras pasadas por la api de gatos
+
+paso 1: Llamar a la api de Swagger para mostrar los hechos de los gatos.
+paso 2: Crear un useEffect() para que la api tan solo se puede llamar una ves cada ves que se renderize el programa
+paso 3: Manejar el llamado de la api utilizando una funcion async-await
+paso 4: Crear un estado para pasarle el dato de la api
+Paso 5: Llamar a la api giphy dentro de otra funcion en el useEffect()
+paso 6: Pasarle las primeras 3 palabras de las frases de la api cat a traves de su busqueda de endpoint
+Paso 7: Darle estilos a la pagina que este centrando en la pantalla, que muestre la frase arriba y abajo el gif;
+
+APi gif: api.giphy.com/v1/gifs/search
+APi cat: 'https://catfact.ninja/fact'
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function App() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
+  const [isFact, setIsFact] = useState('');
+  const [search, setSearch] = useState('');
+  const [isGift, setIsGift] = useState([]);
+  useEffect(() => {
+    async function catFact() {
+      const res = await fetch('https://catfact.ninja/fact');
+      const data = await res.json();
+      setIsFact(data.fact);
+      setSearch(data.fact.split(" ",3).join(' '));
+    }
+   
+    catFact();
+
+  },[])
+  useEffect(() => {
+    async function git() {
+      const res = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${search}`);      
+      const data = await res.json();
+      setIsGift(data?.data[0]?.images.original.url)
+    }
+    git();
+  } ,[search])
   return (
-    <div>App</div>
+    <div className='container'>
+      <div className='fact'>
+        <p>{isFact}</p>
+      </div>
+      <figure className='gif'>
+        <img src={isGift} alt="" />
+
+      </figure>
+    </div>
   )
 }
 
